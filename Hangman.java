dbb855e1430fd595eac1ce648fede6009fee1e19
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2019-10-09 10:15:19 -0400

    1.2

--- Hangman.java
+++ Hangman.java
@@ -1,4 +1,9 @@
+/*
+ * Created by Zachary Niles Peretz
+ * Last Updated 10/09/2019
+ */
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Hangman {
 
@@ -12,38 +17,55 @@ public class Hangman {
 	int correctGuesses = 0; //initializes correct guesses
 	String display = "";
 	
-	for (int i = 1; i < word.length(); i++) { //print black spaces
-		display += "_ "; }
+	for (int i = 1; i < word.length(); i++) { //print blank spaces
+		display += "_ ";
+	}
 	System.out.println(display);
+	
+	ArrayList<String> guessedLetters = new ArrayList<String>(); //create list of guessed letters
 	
 	Scanner yourGuess = new Scanner(System.in); //ask for a guess
 	while (mistakes < 6 && correctGuesses < word.length()) { 
 		System.out.println("What is your guess?");	
 		String guess = yourGuess.nextLine();
+		
+		if (guessedLetters.contains(guess)) { //checks to see if guess is on list, but doesn't actually stop it from being checked
+			System.out.println("You've already guessed that");
+		}
 	
-		int position = word.indexOf(guess); //counting mistakes
-		if (position == -1) {
-			mistakes++;	}
-		else
+		int position = word.indexOf(guess);
+		int positionAlreadyGuessed = -1;
+		if (!(guessedLetters.isEmpty())) {
+			positionAlreadyGuessed = word.indexOf(guessedLetters.get(0));
+		}
+		
+		if (position == -1) {  //counting mistakes
+			mistakes++;
+		}
+		else {
 			correctGuesses++; //counting correct guesses
-	
+		}
+		
 		String newDisplay = "";
 	
-		for (int i = 0; i < word.length(); i++) //I think this bit is the problem
+		for (int i = 0; i < word.length(); i++) {
 			if (i == position) {
-				newDisplay += word.charAt(i);
-				newDisplay += display.charAt(1); } //print guessed letter
+				newDisplay += word.charAt(i); //print guessed letter
+				newDisplay += " ";
+			} 
+			if (i == positionAlreadyGuessed) {
+				newDisplay +=word.charAt(i); //print letters already guessed
+				newDisplay += " ";
+			}
 			else {
-				newDisplay += display.charAt(0); //print '_'
-				newDisplay += display.charAt(1); }
-		
+				newDisplay += "_ ";
+			}
+		}
 		System.out.println(newDisplay);
-	//use array and boolean value
-	//if boolean is true, add letter from array to new display, otherwise, add new letter
-		
-	
+		guessedLetters.add(guess); //adds guess to list
 	}
 	yourGuess.close();
 	myWord.close();
+
 	}
 }
